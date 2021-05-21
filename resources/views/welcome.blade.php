ea409c6a2ba1dd6d18723b07cfb63f6645ec276c
Ricardo <ricardogrijalva10@gmail.com>
2021-05-21 17:38:34 -0600

    Deleting data

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,13 @@
 @section('header')
     <div class="top-div container-fluid">
         <div class="row">
-            <div class="col-12">
-                <p>{{ session('message') }}</p>
-            </div>
+            {{-- if delete action was campleed successfully --}}
+            @if (session('message'))
+                <div class="alert alert-success alert-dismissible text-center fade show" role="alert">
+                    {{ session('message') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
             <div class="my-auto col-12 col-sm-12 col-md-6 order-2 order-sm-2 order-md-1">
                 <p class="fs-3 slogan mt-5">Best Food <span class="sloganYellow">of local restaurants.</span> </p> 
                 <p class="top-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur magni sint voluptatibus aliquam vitae praesentium sunt accusamus fugiat labore. Numquam voluptatem beatae corporis animi harum ab corrupti quia suscipit libero.</p>
